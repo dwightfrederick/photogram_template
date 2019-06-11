02a363b4b64a44b5f32d7f137bb1f2336b1e0f08
Raghu Betina <raghu@firstdraft.com>
2019-06-11 02:46:53 +0000

    Whitelist columns in Trip for ActiveAdmin

--- app/admin/trips.rb
+++ app/admin/trips.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Trip do
+
+ permit_params :start_date, :end_date, :user_id, :location
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
